06249d0559e26fce42954c625d2bf202ec5c4073
Mitsevich Maxim <max_mitsevich@mail.ru>
2023-09-30 19:33:56 +0300

    add exceptions

--- src/base/external_includes/exceptions/database_exceptions.h
+++ src/base/external_includes/exceptions/database_exceptions.h
@@ -5,3 +5,4 @@
 PROJECT_EXCEPTION(DatabaseException, BaseException, "(Database error) ")
 PROJECT_EXCEPTION(DatabaseConnectException, DatabaseException, "(Database connection error) ")
 PROJECT_EXCEPTION(DatabaseExecutionException, DatabaseException, "(Database execution error) ")
+PROJECT_EXCEPTION(DatabaseNotFoundException, DatabaseException, "(Database not found) ")
--- src/base/external_includes/exceptions/logic_exceptions.h
+++ src/base/external_includes/exceptions/logic_exceptions.h
@@ -3,6 +3,6 @@
 #include "base_exception.h"
 
 PROJECT_EXCEPTION(LogicException, BaseException, "(Logic error)")
+PROJECT_EXCEPTION(NotInitializedException, LogicException, "(Not initialized member)")
 PROJECT_EXCEPTION(InvalidAuthDataException, LogicException, "(Invalid auth data)")
-PROJECT_EXCEPTION(RecomendationStrategyException, LogicException, "(Recomendation Strategy)")
-PROJECT_EXCEPTION(CritertionException, RecomendationStrategyException, "(Criterion)")
+PROJECT_EXCEPTION(PersonNotFoundException, LogicException, "(Person not found)")
