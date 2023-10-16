5e9928fa83f0e56a46a874352625cf04bc11e034
Mitsevich Maxim <max_mitsevich@mail.ru>
2023-10-17 00:21:19 +0300

    test

--- postgres/30-create_test_database.sh
+++ postgres/30-create_test_database.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql "user=postgres hostaddr=127.0.0.1 port=5432 password=postgres" < scripts/db-v3.sql
-psql "user=postgres hostaddr=127.0.0.1 port=5432 password=postgres" < cars/crete_tables.sql
-psql "user=postgres hostaddr=127.0.0.1 port=5432 password=postgres" < payments/crete_tables.sql
-psql "user=postgres hostaddr=127.0.0.1 port=5432 password=postgres" < rentals/crete_tables.sql
+psql "user=postgres hostaddr=127.0.0.1 port=5432 password=postgres" < cars/create_tables.sql
+psql "user=postgres hostaddr=127.0.0.1 port=5432 password=postgres" < payments/create_tables.sql
+psql "user=postgres hostaddr=127.0.0.1 port=5432 password=postgres" < rentals/create_tables.sql
 psql "user=postgres hostaddr=127.0.0.1 port=5432 password=postgres" < cars/insert_test_car.sql
